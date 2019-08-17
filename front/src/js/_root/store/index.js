f81283f1dd51ab73aa549beeb37c696fa317adf6
Andrei Zenkou <Andrei.Zenkou@itechart-group.com>
2019-08-17 13:20:51 +0300

    Fix

--- front/src/js/_root/store/index.js
+++ front/src/js/_root/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import initialState from '_root/constants/initial-state';
@@ -8,8 +8,11 @@ export default () => {
     const store = createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk),
-        compose(window.devToolsExtension ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)
+
+        compose(
+            window.devToolsExtension ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+            applyMiddleware(thunk)
+        )
     );
 
     return store;
