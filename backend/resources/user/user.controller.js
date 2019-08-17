81b3f09a743a139c620ee0bfa335bb2873ec4681
Max-Starling <17.max.starling@gmail.com>
2019-08-17 17:20:26 +0300

    Implement user sign in and sign up functionality

--- backend/resources/user/user.controller.js
+++ backend/resources/user/user.controller.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcrypt');
+
+const jwtService = require('../../services/jwtService');
 const userService = require('./user.service');
 
 module.exports.getUsers = async (req, res) => {
@@ -23,8 +26,57 @@ module.exports.getUserById = async (req, res) => {
 };
 
 module.exports.signIn = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      throw new Error('Username and password are required!');
+    }
+    const user = await userService.findOne({ username });
+    if (!user) {
+      throw new Error('User doesn\'t exist!');
+    }
+
+    const match = await bcrypt.compare(password, user.passwordHash);
+    if (!match) {
+      throw new Error('Wrong password!');
+    }
+    const token = jwtService.sign({ username: user.username });
+    res.status(200);
+    res.send({ user, token });
+  } catch (e) {
+    res.status(404);
+    res.send(e.message);
+  }
 };
 
 module.exports.signUp = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      throw new Error('Username and password are required!');
+    }
+    const user = await userService.findOne({ username });
+    if (user) {
+      throw new Error('User is already exist!');
+    }
 
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    if (!passwordHash) {
+      throw new Error('Cannot hash password!');
+    }
+    
+    const createdUser = userService.create({ username, passwordHash });
+
+    const token = jwtService.sign({ username: createdUser.username });
+    res.status(200);
+    res.send({ user: createdUser, token });
+    throw new Error('Wrong password!');
+  } catch (e) {
+    res.status(404);
+    res.send(e.message);
+  }
 };
