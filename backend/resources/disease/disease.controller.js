9955698857d3dac020a6672ff65d79527b1ac58d
MaksimZhukau <maks_zhukov-97@mail.ru>
2019-08-18 00:03:40 +0300

    added get article by disease

--- backend/resources/disease/disease.controller.js
+++ backend/resources/disease/disease.controller.js
@@ -1,4 +1,5 @@
 const diseaseService = require('./disease.service');
+const articleService = require('../article/article.service');
 
 module.exports.getDiseases = async (req, res) => {
   try {
@@ -9,3 +10,15 @@ module.exports.getDiseases = async (req, res) => {
     console.log(e);
   }
 };
+
+module.exports.getDiseaseById = async (req, res) => {
+    const { diseaseId } = req.params;
+    try {
+      const [{ articleId }] = await diseaseService.find({ _id: diseaseId });
+      const data = await articleService.find({ _id: articleId });
+      res.status(200);
+      res.send([...data]);
+    } catch(e) {
+      console.log(e);
+    }
+  };
--- backend/resources/disease/index.js
+++ backend/resources/disease/index.js
@@ -4,5 +4,6 @@ const controller = require('./disease.controller');
 const router = new Router();
 
 router.get('/', controller.getDiseases);
+router.get('/:diseaseId', controller.getDiseaseById);
 
 module.exports = router;
