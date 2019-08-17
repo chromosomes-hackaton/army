a553228299c9455a0433da2689aa32ba7caae1be
Max-Starling <17.max.starling@gmail.com>
2019-08-17 17:19:53 +0300

    Implement jwt service

--- backend/config/index.js
+++ backend/config/index.js
@@ -2,14 +2,22 @@ const port = parseInt(process.env.PORT, 10) || 3002;
 
 const env = process.env.NODE_ENV || 'development';
 
+const jwtConfig = ({
+  secret: 'jwt chromosomes dev server',
+  options: {
+    expiresIn: '3h',
+  },
+});
+
 const config = {
   development: {
     apiUrl: `http://localhost:${port}`,
     port,
     mongoDB: {
-      name: 'bump',
+      name: 'army',
       url: 'mongodb+srv://John:fafafa@cluster0-bpvhy.mongodb.net?retryWrites=true&w=majority',
     },
+    jwtConfig,
   },
 };
 
